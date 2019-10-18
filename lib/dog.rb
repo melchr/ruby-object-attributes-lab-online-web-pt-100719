8e323d6cb941304133fcdac5b1fb9295988ec1e0
Melissa Christman <melchrstmn@gmail.com>
2019-10-18 01:53:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,14 +3,14 @@ class Dog
     @name = dog_name
   end
   def name
-    
+    @name
   end
   
   def breed=(dog_breed)
     @breed = dog_breed
   end
   def breed
-    @breed = dog_breed
+    @breed
   end
   
 end
